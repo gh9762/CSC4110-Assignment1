54b0a3c1c9358ccb9cd37088b52259c598ee1d45
Dylan Denha <gh9762@wayne.edu>
2020-09-20 17:47:48 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Dylan Denha <gh9762@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
